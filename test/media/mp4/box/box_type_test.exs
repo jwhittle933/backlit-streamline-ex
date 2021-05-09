8546815ecc182cc693ddb3cc25b39623212d6583
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-05-09 15:05:31 -0400

    add sample mp4, boxType test

--- test/media/mp4/box/box_type_test.exs
+++ test/media/mp4/box/box_type_test.exs
@@ -10,4 +10,12 @@ defmodule BoxTypeTest do
     assert name == "ftyp"
     assert rest == <<1, 3, 4>>
   end
+
+  test "invalid length" do
+    header = <<102, 116>>
+    {name, rest} = BoxType.from(header)
+
+    assert name == "unknown"
+    assert rest == <<102, 116>>
+  end
 end
